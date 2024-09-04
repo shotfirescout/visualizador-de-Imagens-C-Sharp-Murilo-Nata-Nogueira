009bb0f1470741cb992799c430cbe3d09d8f9da5
murilonat nogueira <murilonata376@gmail.com>
2024-09-04 20:00:43 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace visualizador_de_Imagens__C_Sharp_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagemSelecionada = new System.Windows.Forms.PictureBox();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemSelecionada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagemSelecionada
+            // 
+            this.imagemSelecionada.Location = new System.Drawing.Point(12, 41);
+            this.imagemSelecionada.Name = "imagemSelecionada";
+            this.imagemSelecionada.Size = new System.Drawing.Size(564, 594);
+            this.imagemSelecionada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagemSelecionada.TabIndex = 0;
+            this.imagemSelecionada.TabStop = false;
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregar.Location = new System.Drawing.Point(614, 88);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(175, 102);
+            this.btnCarregar.TabIndex = 1;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(614, 320);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(175, 102);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Exibidor de Imagens dentro do sistema";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 633);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.imagemSelecionada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imagemSelecionada)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagemSelecionada;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace visualizador_de_Imagens__C_Sharp_
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private Bitmap imageBitmap;
+        private String localArquivo;
+        public Form1()
+        {
+            InitializeComponent();
+            configurarFileDialog();
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            abrirArquivo();
+        }
+
+        private void configurarFileDialog()
+        {
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures); //Essa é o diretório que vai começar
+            openFileDialog.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF"; //Esse é um filto para usar
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+        }
+
+        private void abrirArquivo()
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    localArquivo = openFileDialog.FileName;
+                    imageBitmap = new Bitmap(localArquivo);
+                    imagemSelecionada.Image = imageBitmap;
+
+                } catch(FileLoadException e)
+                {
+                    MessageBox.Show("Arquivo não encontrado");
+                }
+                
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            if (imagemSelecionada != null)
+            {
+                imagemSelecionada.Image.Dispose();
+                imagemSelecionada.Image = null;
+            } else
+            {
+                MessageBox.Show("A imagem não foi selecionada ainda");
+            }
+
+        }
+    }
+}
